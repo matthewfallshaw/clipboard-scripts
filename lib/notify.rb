d44cc6a3c0a802ea10ade89adf3d84703c4a45b4
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-09-02 10:30:43 +1000

    support sticky notifications make rot13 notifications sticky

--- lib/notify.rb
+++ lib/notify.rb
@@ -1,5 +1,5 @@
-def notify(message)
+def notify(message, sticky = false)
   require 'shellwords'
-  `/usr/local/bin/growlnotify -a Quicksilver -m #{message.shellescape}`
+  `/usr/local/bin/growlnotify#{sticky ? " -s" : ""} -a Quicksilver -m #{message.shellescape}`
   message
 end
