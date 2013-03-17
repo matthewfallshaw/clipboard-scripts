88df4c24b0f51ae63a1a1124ec957251ec063bf3
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2013-03-17 19:31:44 +1100

    bugfix: require required library

--- pb-humanize.sh
+++ pb-humanize.sh
@@ -2,8 +2,10 @@
 # Sentence caserize the contents of the OSX clipboard
 begin
   require_relative("../lib/helper")
+  require_relative("../lib/humanize")
 rescue
   require File.join(File.dirname(__FILE__), "../lib/helper")
+  require File.join(File.dirname(__FILE__), "../lib/humanize")
 end
 
 clipboard {|c| HumanizingString.new(c).humanize }
