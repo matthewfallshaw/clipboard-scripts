f1f9a93a8a09e629a45ccd1b356fb0239faa2dde
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2013-03-19 06:01:54 +1100

    bugfix: move hashbang to top of file

--- pb-qrencode.sh
+++ pb-qrencode.sh
@@ -1,6 +1,6 @@
+#!/bin/sh
 # Generate and display QR-code from clipboard
 # Mac only
 
-#!/bin/sh
-pbpaste | qrencode -o /tmp/qr.png
+/usr/bin/pbpaste | /usr/local/bin/qrencode -o /tmp/qr.png
 qlmanage -p 2>/dev/null /tmp/qr.png
